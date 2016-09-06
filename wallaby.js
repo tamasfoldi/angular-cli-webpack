1f37acc80eb10f7809b46fb1b0cf571b7e43527e
Tamas Foldi <bucicimaci@gmail.com>
2016-09-06 10:34:56 +0200

    wallaby config mods for more realistic coverage report

--- wallaby.js
+++ wallaby.js
@@ -8,14 +8,14 @@ var webpackPostprocessor = wallabyWebpack({
 
   module: {
     loaders: [
-      {test: /\.css$/, loader: 'raw-loader'},
-      {test: /\.html$/, loader: 'raw-loader'},
-      {test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/},
-      {test: /\.json$/, loader: 'json-loader'},
-      {test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader']},
-      {test: /\.less$/, loaders: ['raw-loader', 'less-loader']},
-      {test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader']},
-      {test: /\.(jpg|png)$/, loader: 'url-loader?limit=128000'}
+      { test: /\.css$/, loader: 'raw-loader' },
+      { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.js$/, loader: 'angular2-template-loader', exclude: /node_modules/ },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.styl$/, loaders: ['raw-loader', 'stylus-loader'] },
+      { test: /\.less$/, loaders: ['raw-loader', 'less-loader'] },
+      { test: /\.scss$|\.sass$/, loaders: ['raw-loader', 'sass-loader'] },
+      { test: /\.(jpg|png)$/, loader: 'url-loader?limit=128000' }
     ]
   }
 });
@@ -26,21 +26,23 @@ module.exports = function (wallaby) {
 
   return {
     files: [
-      {pattern: 'src/**/*.ts', load: false},
-      {pattern: 'src/**/*.css', load: false},
-      {pattern: 'src/**/*.html', load: false},
-      {pattern: 'src/**/*spec.ts', ignore: true}
+      { pattern: 'src/**/*.ts', load: false },
+      { pattern: 'src/**/*.css', load: false },
+      { pattern: 'src/**/*.html', load: false },
+      { pattern: 'src/**/*spec.ts', ignore: true },
+      { pattern: 'src/test.ts', ignore: true },
+      { pattern: 'src/main.ts', ignore: true }
     ],
 
     tests: [
-      {pattern: 'src/**/*spec.ts', load: false}
+      { pattern: 'src/**/*spec.ts', load: false }
     ],
 
     testFramework: 'jasmine',
 
     env: {
       runner: require('phantomjs-prebuilt').path,
-      params: {runner: '--web-security=false'}
+      params: { runner: '--web-security=false' }
     },
 
     compilers: {
