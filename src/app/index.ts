1c6dbd7b731bfba2395ba2efcae2d94264381bf3
Tamas Foldi <bucicimaci@gmail.com>
2016-09-13 20:14:16 +0200

    fix

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,7 +2,6 @@ export * from './actions';
 export * from './effects';
 export * from './interfaces';
 export * from './reducers';
-// export * from './pages';
 export * from './services';
 export * from './components';
 export * from './app.module';
